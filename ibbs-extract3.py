b56ab69c59114417f142659366c0fc02df8285e9
xenos <xenos@aliens.ph>
2023-02-11 02:48:20 +0800

    Fixed ibbs-extract3 to spawn wwivutil to dump the appropriate message bases for processing to generate laston.txt. Reading the message.dat file directly wasn't working because it didn't map properly with message sequences and contained even the deleted messages. Renamed ibbs-extract3 to ibbs-extract3.py so it's easier to run in Windows.

--- ibbs-extract3.py
+++ ibbs-extract3.py
@@ -20,13 +20,12 @@ try:
   from progress.bar import Bar
 except ImportError:
   Bar=False
-
+######################################################################
 # read in wwiv.ini and replace the defaults
 config=configparser.ConfigParser(allow_no_value=True)
 config.read("wwiv.ini")
 subs=config['wwiv5ibbslastcaller'].get('subs',"IBBSDAT,FSX_DAT").split(',')
 display=int(config['wwiv5ibbslastcaller'].get('display','11'))
-
 
 ######################################################################
 def get_host(subname):
@@ -50,7 +49,6 @@ def rot47(s):
       l+=chr(k)
   return str(l)
 
-
 wwiv=json.load(open("config.json","r"))
 networkfile=f"{wwiv['config']['datadir']}networks.json"
 n=json.load(open(networkfile,"r"))
@@ -60,10 +58,10 @@ for sub in subs:
   if tosys>=0:
     networkname=f"{n['networks'][net]['name']}"
     networkdir=f"{n['networks'][net]['dir']}"
-    filename=f"{wwiv['config']['msgsdir']}{filename}.dat"
     s=""
     l=""
-    d=codecs.open(filename,"r","latin_1") # read in messages 
+    o=subprocess.run(["wwivutil","messages","dump",filename],capture_output=True)
+    lines=o.stdout.decode("latin_1")
     e=open(f"{networkdir}laston.txt","w+b") # get ready to write
     es="|#4%sInterBBS Last Callers for: %s%s|#0\r\n" % (" "*(29-len(networkname)), networkname, " "*(29-(len(networkname)-(len(networkname)%2))))
     e.write(es.encode("latin_1"))
@@ -71,16 +69,15 @@ for sub in subs:
     e.write(es.encode("latin_1"))
     es="|#7\xC3"+("\xC4"*13) +"\xC5"+("\xC4"*6)+"\xC5"+("\xC4"*8)+"\xC5"+("\xC4"*24)+"\xC5"+("\xC4"*22)+"\xB6\r\n"
     e.write(es.encode("latin_1"))
-    lines=d.read()
-    count=len(re.findall(">>> BEGIN\r\n",lines))
+    count=len(re.findall(">>> BEGIN",lines))
     start=0
     emit=0
     x=0
-    d.seek(0)
+    y=""
     if Bar:
       bar=Bar(f"Munching on {networkname}...", max=count)
-    for line in d:
-      if line==">>> END\r\n":
+    for line in lines.split("\r\n"):
+      if line==">>> END":
         emit=-1
         x=0
         y=s.split("\n")
@@ -92,9 +89,9 @@ for sub in subs:
             break
         s=""
       if emit==1:
-        s=str(s)+rot47(line)
+        s=str(s)+rot47(line)+"\n"
         x+=1
-      if line==">>> BEGIN\r\n":
+      if line[-9:]==">>> BEGIN":
         if Bar:
           bar.next()
         start+=1
